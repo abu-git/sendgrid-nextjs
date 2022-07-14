f324461a0c9dc81e9d387b3bfe6ae24e26aaa128
abu-git <abu.sammie@gmail.com>
2022-07-14 14:19:49 +0200

    contact form progress

--- components/Contact.js
+++ components/Contact.js
@@ -7,12 +7,24 @@ export default function Contact(){
     const [message, setMessage] = useState('')
 
     return (
-        <form
-            className='w-[650px] mt-10 rounded-lg shadow-xl flex flex-col px-8 py-8 dark:bg-blue-500'
-        >
+        <form className='w-[650px] mt-10 rounded-lg shadow-xl flex flex-col px-8 py-8 dark:bg-blue-500'>
+            
             <h1 className='text-2xl font-bold dark:text-gray-50'>Send Message</h1>
-            <label htmlFor='fullname' className='text-gray-500 light mt-8 dark:text-gray-50'>
+
+
+            <label htmlFor='fullname' className='text-gray-500 font-light mt-8 dark:text-gray-50'>
                 Full name<span className='text-red-500 dark:text-gray-50'>*</span>
+            </label>
+            <input 
+                type='text'
+                value={fullname}
+                onChange={(e) => { setFullname(e.target.value)}}
+                name='fullname'
+                className='bg-transparent borber-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500' 
+            />
+
+            <label htmlFor='email' className='text-gray-500 font-light mt-4 dark:text-gray-50'>
+                Email<span className='text-red-500 dark:text-gray-50'>*</span>
             </label>
         
         </form>
